60dfe107002f3654a6f65c301dcaabcc26769aa0
adharesh <arti.dhareshwar21@gmail.com>
2023-11-02 20:51:23 -0400

    deleted unused

--- src/javascripts/art(i).js
+++
@@ -1,7 +0,0 @@
-function openImage() {
-    document.getElementById("enlarged-image").style.display = "block";
-}
-
-function closeImage() {
-    document.getElementById("enlarged-image").style.display = "none";
-}
